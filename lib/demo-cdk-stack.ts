e7e29907626328d9bf32e4ca3988637f2a4092d9
Bikesh Shrestha <bikesh@sharelookapp.com>
2023-05-03 14:27:47 +0545

    service role added

--- lib/demo-cdk-stack.ts
+++ lib/demo-cdk-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DemoCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 👇 Create ACM Permission Policy
+    const describeAcmCertificates = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          resources: ['arn:aws:acm:*:*:certificate/*'],
+          actions: ['acm:DescribeCertificate'],
+        }),
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+
+    // 👇 Create Role
+    const role = new iam.Role(this, 'example-iam-role', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+      description: 'An example IAM role in AWS CDK',
+      inlinePolicies: {
+        DescribeACMCerts: describeAcmCertificates,
+      },
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'AmazonAPIGatewayInvokeFullAccess',
+        ),
+      ],
+    });
+
+    new iam.Role(this, 'Role', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      description: 'shlbot role opensearch',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaVPCAccessExecutionRole'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')
+      ],
+    });
   }
 }
+
